e79ae89e98fd161d8aaf9146840e41b9437cbb6e
Yannick Kollega <ykollega@gmail.com>
2022-12-12 15:35:17 +0100

    refactor: enhance scss structure and initiate reusable alignment-container mixin

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,9 +5,7 @@ import * as styles from './styles.module.scss';
 
 export const Layout = ({ children }: { children?: React.ReactNode }) => (
     <>
-        <body className={styles.root}>
-            <TopBar />
-            <div className={styles.container}>{children}</div>
-        </body>
+        <TopBar />
+        <div className={styles.container}>{children}</div>
     </>
 );
