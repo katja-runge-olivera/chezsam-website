a4714f394d5d9c58de54d5e4d53a9642a521d89e
KatRunge <katja.runge@jimdo.com>
2022-12-18 17:25:41 +0100

    fix: css of hero section

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,8 +4,8 @@ import { TopBar } from '../TopBar';
 import * as styles from './styles.module.scss';
 
 export const Layout = ({ children }: { children?: React.ReactNode }) => (
-    <>
+    <React.Fragment>
         <TopBar />
         <div className={styles.container}>{children}</div>
-    </>
+    </React.Fragment>
 );
